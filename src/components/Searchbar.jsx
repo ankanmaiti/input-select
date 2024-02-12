b35b47aa24d8939003121952eec39e777444ab04
ankanmaiti <ankanmaiti2@gmail.com>
2024-02-12 16:37:52 +0530

    add dialog tag

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 
-export default function Searchbox({ options }) {
-  const [input, setInput] = useState("");
-  const [showDropdown, setShowDropdown] = useState(false);
-
+function FilteredInputs({ input, option, onClick }) {
   function doesMatchedWithInput(option) {
     return option.toLowerCase().includes(input.toLowerCase());
   }
 
+  if (!doesMatchedWithInput(option)) return null;
+
+  return (
+    <button
+      onClick={onClick}
+      className="w-full bg-white rounded text-left px-2 py-1 hover:bg-blue-300 hover:text-white"
+    >
+      {option}
+    </button>
+  );
+}
+
+export default function Searchbox({ options }) {
+  const [input, setInput] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+
   function handleInputChange(e) {
     setInput(e.target.value);
-    setShowDropdown(true);
+    showDropdown || setShowDropdown(true);
   }
 
-  function handleCurrencySelect(option) {
+  function handleSelect(option) {
     setInput(option);
     setShowDropdown(false);
   }
@@ -29,23 +42,16 @@ export default function Searchbox({ options }) {
         onChange={handleInputChange}
       />
 
-      {showDropdown && (
-        <ul className="bg-white rounded w-full absolute">
-          {options.map((option) => {
-            if (!doesMatchedWithInput(option)) return null;
-            return (
-              <li key={option}>
-                <button
-                  className="w-full bg-white rounded text-left px-2 py-1 hover:bg-blue-300 hover:text-white"
-                  onClick={() => handleCurrencySelect(option)}
-                >
-                  {option}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+      <dialog open={showDropdown} className="bg-white rounded w-full absolute">
+        {options.map((option) => (
+          <FilteredInputs
+            key={option}
+            option={option}
+            input={input}
+            onClick={() => handleSelect(option)}
+          />
+        ))}
+      </dialog>
     </div>
   );
 }
